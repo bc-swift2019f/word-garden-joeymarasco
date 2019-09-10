a5082ae10a4bcf6c66ba4df65ed471f6f00112bf
Joseph Marasco <marascoj@bc.edu>
2019-09-10 14:11:39 -0400

    3.2 completed, UI has been linked with the view controller

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -12,9 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("In viewDidLoad, is guessed letter field the first responder?", guessedLetterField.isFirstResponder)
     }
 
+ 
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UILabel!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
+   
+    
+    func updateUIAfterGuess(){
+        guessedLetterField.text = ""
+        guessedLetterField.resignFirstResponder()
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func guessLetterButtonPressed(_ sender: Any) {
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func plagAgainButtonPressed(_ sender: Any) {
+
+    }
+
+    @IBAction func doneKeyPressed(_ sender: Any) {
+        updateUIAfterGuess()
+    }
+    
 
 }
+
 
