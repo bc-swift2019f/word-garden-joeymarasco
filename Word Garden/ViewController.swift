405882101d8425c98402612f38bf4c103445b3a6
Joseph Marasco <marascoj@bc.edu>
2019-09-10 18:58:30 -0400

    3.3 completed, play again button has been hidden from viewDidLoad. Guess letter button has been disabled from viewdidload

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("In viewDidLoad, is guessed letter field the first responder?", guessedLetterField.isFirstResponder)
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
     }
 
  
@@ -20,7 +22,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var guessedLetterField: UITextField!
     @IBOutlet weak var guessLetterButton: UIButton!
     @IBOutlet weak var guessCountLabel: UILabel!
-    @IBOutlet weak var playAgainButton: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
     @IBOutlet weak var flowerImageView: UIImageView!
     
    
@@ -31,6 +33,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func guessedLetterFieldChanged(_ sender: Any) {
+        if let letterGuessed = guessedLetterField.text?.last{
+            guessedLetterField.text = "\(letterGuessed)"
+            guessLetterButton.isEnabled = true
+        } else {
+            // disable the button, if i dont have a single character in the guessed letter field
+            guessLetterButton.isEnabled = false
+        }
     }
     
     
